ba92465166a64d1aef3dbcaf382940358ab2c645
Simona Stamenkova <simona_stamenkova@hotmail.com>
2017-07-05 17:42:00 +0200

    - added professor and document services

---
+++ soa-document-service/src/main/java/edu/soa/iknow/SoaDocumentServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.soa.iknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaDocumentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaDocumentServiceApplication.class, args);
+	}
+}
---
+++ soa-professor-service/src/main/java/edu/soa/iknow/SoaProfessorServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.soa.iknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaProfessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaProfessorServiceApplication.class, args);
+	}
+}
